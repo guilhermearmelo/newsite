81c4c4b0b7dba8ee2d8c5036a9edd923dd7939cd
Guilherme A. Melo <guilhermearmelo@gmail.com>
2019-02-15 22:53:38 -0200

    Creating first usable version of the website

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="db dt-l w-100 border-box pa3 ph5-l bb">
+            <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+                GUILHERME MELO
+            </a>
+            <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
+                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
+                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Tecnologies">Portfolio</a>
+                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Portfolio">Tecnologies</a>
+                <a className="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
+            </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
