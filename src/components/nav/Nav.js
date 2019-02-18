6408f3aa1b8493e65c252987a31f23ab88b9802a
Guilherme A. Melo <guilhermearmelo@gmail.com>
2019-02-18 12:31:12 -0300

    Linking images of resources

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div>
-            <nav className="db dt-l w-100 border-box pa3 ph5-l bb">
-            <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+        <div id="home">
+            <nav className="db dt-l w-100 border-box pa3 ph5-l bb bg-black-90">
+            <a className="db dtc-l v-mid white link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#home" title="Home">
                 GUILHERME MELO
             </a>
             <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
-                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Tecnologies">Tecnologies</a>
-                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#Portfolio" title="Portfolio">Portfolio</a>
-                <a className="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
+                <a className="link dim white f6 f5-l dib mr3 mr4-l" href="#home" title="Home">Home</a>
+                <a className="link dim white f6 f5-l dib mr3 mr4-l" href="#tecnologies" title="Tecnologies">Tecnologies</a>
+                <a className="link dim white f6 f5-l dib mr3 mr4-l" href="#portfolio" title="Portfolio">Portfolio</a>
+                <a className="link dim white f6 f5-l dib" href="#" title="Contact">Contact</a>
             </div>
             </nav>
         </div>
